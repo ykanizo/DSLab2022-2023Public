ea9106dcecfecfe1e056f6b595d345ecd7dd0352
michalh <michalh@192.168.2.1>
2019-11-11 12:12:40 +0200

    update

--- DS-Lab04-DLinkedList/src/ListTest.java
+++ DS-Lab04-DLinkedList/src/ListTest.java
@@ -1,21 +1,35 @@
 import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
 
+@RunWith(Parameterized.class)
 public abstract class ListTest<T> {
 
-	private DListFactory<T> dlistFactory = new DListFactory<T>();
+	@Parameter
+	public String listClassName;
+	private DListFactory<T> dListFactory = new DListFactory<T>();
 	private List<T> dList;
 
-	@Before
-	public void setUp() throws Exception {
-		dList = dlistFactory.getDList();
+	@Parameterized.Parameters
+	public static Iterable<String> classNames() {
+		return DListFactory.getClassNames();
 	}
-
 	
 	/**
-	 * @return a new instance of parameter T. 
-	 * Two instances which are created by this method should be different according to "equals" method.
-	 * For example for T=Object, the implementation may be "return new Object();". 
+	 * @return a new instance of parameter T. Two instances which are created by
+	 *         this method should be different according to "equals" method. For
+	 *         example for T=Object, the implementation can be "return new
+	 *         Object();".
 	 */
 	public abstract T getParameterInstance();
 
+	@Before
+	public void setUp() throws Exception {
+		dList = dListFactory.getDList(listClassName);
+		// TODO You can add your code here
+	}
+	
+	// TODO You should add @Test methods here
+	
 }
