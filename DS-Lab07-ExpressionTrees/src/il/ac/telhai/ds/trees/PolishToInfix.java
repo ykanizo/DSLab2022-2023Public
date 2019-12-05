78a350e71602b4da5762718c7abd9e6ac0ebfc0a
michalh <michalh@192.168.1.2>
2019-12-05 16:05:43 +0200

    update

---
+++ DS-Lab07-ExpressionTrees/src/il/ac/telhai/ds/trees/PolishToInfix.java
@@ -0,0 +1,17 @@
+package il.ac.telhai.ds.trees;
+
+import java.io.*;
+
+public class PolishToInfix {
+
+	public static void main(String[] args) throws IOException {
+		StreamTokenizer tokenizer = new StreamTokenizer(new InputStreamReader(System.in));
+		tokenizer.slashSlashComments(false);
+		tokenizer.ordinaryChar('/');
+		ExpressionTree expr = ExpressionTree.createTree(tokenizer);
+		System.out.println("Infix: " + expr.infix());
+		System.out.println("Prefix: " + expr.prefix());
+		System.out.println("Value: "+expr.evaluate());
+	}
+
+}
