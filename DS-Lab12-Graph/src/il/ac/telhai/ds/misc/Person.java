f5958dd0b27cbbc4d3e0329140aebfa17e154fdc
michalh <michalh@192.168.2.1>
2020-01-18 22:46:06 +0200

    update hashcode

--- DS-Lab12-Graph/src/il/ac/telhai/ds/misc/Person.java
+++ DS-Lab12-Graph/src/il/ac/telhai/ds/misc/Person.java
@@ -41,9 +41,9 @@ public class Person implements Comparable<Person>{
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-//		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
-//		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
 		return result;
 	}
 
@@ -65,13 +65,13 @@ public class Person implements Comparable<Person>{
 			if (other.id != null)
 				return false;
 		} 
-//			else if (!id.equals(other.id))
-//			return false;
-//		if (lastName == null) {
-//			if (other.lastName != null)
-//				return false;
-//		} else if (!lastName.equals(other.lastName))
-//			return false;
+		else if (!id.equals(other.id))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
 		return true;
 	}
 
