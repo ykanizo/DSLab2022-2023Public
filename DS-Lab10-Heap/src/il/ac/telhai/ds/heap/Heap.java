afa1e3aa3633948bd6ae51b6baf404a2a18d89e8
michalHorovitz <michal.horovits@gmail.com>
2021-12-12 10:54:40 +0200

    add Heap

---
+++ DS-Lab10-Heap/src/il/ac/telhai/ds/heap/Heap.java
@@ -0,0 +1,41 @@
+package il.ac.telhai.ds.heap;
+
+public class Heap<T extends Comparable<T>> {
+
+	public Heap(int length) {
+		// TODO add your implementation here
+	}
+
+	public Heap(T[] arr) {
+		// TODO add your implementation here
+	}
+
+	public boolean isFull() {
+		// TODO add your implementation here
+	}
+
+	public boolean isEmpty() {
+		// TODO add your implementation here
+	}
+
+	public void insert(T element) {
+		// TODO add your implementation here
+	}
+
+	public T getMin() {
+		// TODO add your implementation here
+	}
+
+	public T deleteMin() {
+		// TODO add your implementation here
+	}
+
+	/**
+	 * return a string represents the heap. The order of the elements are according
+	 * to The string starts with "[", ends with "]", and the values are seperated
+	 * with a comma
+	 */
+	public String toString() {
+		// TODO add your implementation here
+	}
+}
